2163f784a1aab26b6f18d8285809059a433fea56
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-10-09 16:40:07 +0200

    pokemonCard created

---
+++ pokedex/src/components/pokemonCard.jsx
@@ -0,0 +1,9 @@
+function pokemonCard() {
+  return (
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      <figcaption>BULBIZARRE</figcaption>
+    </figure>
+  );
+}
+export default pokemonCard;
