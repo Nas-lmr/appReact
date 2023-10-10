f98d3a6a21a7d2839373d867afaea1214b5696e0
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-10-10 12:02:56 +0200

    update branch component.2

--- pokedex/src/components/pokemonCard.jsx
+++ pokedex/src/components/pokemonCard.jsx
@@ -1,8 +1,22 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function pokemonCard() {
+  let pokemon = pokemonList[0];
+
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      <figcaption>BULBIZARRE</figcaption>
+    <figure className="card">
+      {/* <img src={pokemon.imgSrc} /> */}
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
